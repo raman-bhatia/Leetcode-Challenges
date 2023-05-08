230e1ca644a7d5413e18a5508115d76ff88f01ca
Ramanpreet Bhatia <i.raman.bhatia@gmail.com>
2023-05-08 16:05:53 -0700

    Time: 43 ms (16.74%), Space: 16.2 MB (13.06%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -2,12 +2,15 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         row = [1] * n
         
-#go through all rows except last one
+#Loop through the rows of the grid, except for the last row.
         for i in range(m-1):
-            newRow = [1] * n
+#Create a new list newRow with n elements that are all 1
+            newRow = [1] * n#
+#Starting from the second last element of newRow and going backwards, compute the number of unique paths to get to that element by adding the number of unique paths to the element to the right and the element below it in the previous row.
             for j in range(n - 2, -1, -1):
                 newRow[j] = newRow[j+1] + row[j]
-                
+#Set row to the newRow for the next iteration. 
             row = newRow
+#Return the first element of the last row, which contains the total number of unique paths to get from the top left corner to the bottom right corner of the grid.
         return row[0]
         
