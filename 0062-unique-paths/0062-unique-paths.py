40f966b51e4978483538925701d9534e3fb9c3fb
Ramanpreet Bhatia <i.raman.bhatia@gmail.com>
2023-05-08 16:08:06 -0700

    Time: 50 ms (7.69%), Space: 16.2 MB (13.06%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -5,7 +5,7 @@ class Solution:
 #Loop through the rows of the grid, except for the last row.
         for i in range(m-1):
 #Create a new list newRow with n elements that are all 1
-            newRow = [1] * n#
+            newRow = [1] * n
 #Starting from the second last element of newRow and going backwards, compute the number of unique paths to get to that element by adding the number of unique paths to the element to the right and the element below it in the previous row.
             for j in range(n - 2, -1, -1):
                 newRow[j] = newRow[j+1] + row[j]
